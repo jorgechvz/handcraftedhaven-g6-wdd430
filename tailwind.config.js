a11b25157dacdb22472f0402644044e7b706d27b
jorgechvz <jorgechvzpon@gmail.com>
2023-11-09 22:39:13 -0500

    Add Tailwind Config

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      silverSand: "#b6b9b7",
+      twine: "#c99861",
+      kumera: "#90630e",
+      mexicanRed: "#ff7849",
+      Kilamanjaro: "#270b04",
+    },
+    fontFamily: {
+      lato: ["Lato", "sans-serif"],
+      poppins: ["Poppins", "sans-serif"],
+      merri: ["Merriweather", "serif"],
+    },
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+    },
+    extend: {
+      spacing: {
+        sm: "8px",
+        md: "12px",
+        lg: "16px",
+        xl: "24px",
+      },
+      borderRadius: {
+        default: "1rem",
+        "4xl": "2rem",
+      },
+    },
+  },
+};
